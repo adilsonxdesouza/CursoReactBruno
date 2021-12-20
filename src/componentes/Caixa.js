1a9442841985dde69d4baf81bae3d5b53fc0beb7
adilsonxavier <adilsonxdesouza@gmail.com>
2021-12-20 02:16:38 -0300

    aula18_contencao

---
+++ src/componentes/Caixa.js
@@ -0,0 +1,17 @@
+﻿import React from "react";
+
+
+export default function Caixa(props) {
+    return (
+        <>
+            <div className="box">
+                {props.children} {/* aqui os componentes filhos que vem do pai são injetados neste ponto */}
+                Email: {props.email}
+                <br />
+                c2: {props.children[3]}   {/*<p>sapo - vim do pai4</p>*/}
+                tamanho: {props.children.length}
+               
+            </div>
+        </>
+        );
+}
