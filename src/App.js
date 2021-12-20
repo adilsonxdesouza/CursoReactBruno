ed5783094dec97c381278e1af0830c1fc094b632
adilsonxavier <adilsonxdesouza@gmail.com>
2021-12-20 03:42:19 -0300

    aula19_useeffect

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 ﻿import React from "react";
-import imagem from "./componentes/imgs/img2.gif";
-//para  importar a imagem precisa incluir o file-loader no webpack.config.js
-
-import Header from "./componentes/Header";
-import Corpo from "./componentes/Corpo"
 
 export default function App() {
     return (
         <>
-            <Header />
-            <Corpo/>
+            useeffect
            
         </>
     );
---
+++ src/AppUseEffect.js
@@ -0,0 +1,28 @@
+﻿import React from "react";
+
+
+// => ler mais sobre o assunto posteriormente
+
+
+export default function AppUseEffect() {
+
+
+    React.useEffect(                 
+        () => {
+            console.log("pagina carregada");
+            document.title = `Contagem: ${contagem}`
+
+        }      //O use effect é chamado quando a pag é carregada ou atualizada
+    );
+
+    const [contagem, setContagem] = React.useState(0);
+
+    return (
+        <>
+            <div>Contagem: {contagem}</div>
+            <button onClick={() => { setContagem(contagem + 1);}}   >clique</button>
+           
+        </>
+    );
+
+} 
