3acedc1209529144313c2383244c305d6ddae09c
adilsonxavier <adilsonxdesouza@gmail.com>
2021-12-20 00:19:46 -0300

    aula16_elevacao

---
+++ src/componentes/Nota.js
@@ -0,0 +1,12 @@
+﻿import React from "react";
+
+export default function Nota(props) {
+    return (
+        <div style={{marginBottom:"1em"} }>
+            <label>Nota {props.num }</label>
+            <input type="text" value={props.nota} onChange={(e) => props.setNota(e.target.value)} />
+            {/* A funçao setNota que está sendo executada é a do componente pai (AppElevacao) */}
+        </div>
+        
+        );
+}
---
+++ src/componentes/Resultado.js
@@ -0,0 +1,12 @@
+﻿import React from "react";
+
+export default function Resultado(props) {
+    return (
+        <div style={{marginBottom:"1em"} }>
+            <label>Soma Notas : { props.somaNotas}</label>
+            <p> {props.somaNotas > 60 ? "Aprovado" : "Reprovado"} </p>
+            {/* A funçao somaMotas que está sendo executada é a do componente pai (AppElevacao) */}
+        </div>
+        
+        );
+}
