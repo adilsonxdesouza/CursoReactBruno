fcd07b2c84a77ad719dadfd03f8c7209af3ae951
adilsonxavier <adilsonxdesouza@gmail.com>
2021-12-21 07:49:04 -0300

    aula23_classstate

--- src/AppClassComp.js
+++ src/AppClassComp.js
@@ -4,7 +4,7 @@ import ClasseCarro from "./componentes/ClasseCarro";
 export default function AppClassComp() {
     return (
         <>
-            <ClasseCarro canal="meucanal">
+            <ClasseCarro canal="meucanal" fator={1}>  {/*Como eu quero passar um número e não string passo o valor de fator entre chaves*/}
                     <p>vim do pai</p>
             </ClasseCarro>
            
--- src/componentes/ClasseCarro.js
+++ src/componentes/ClasseCarro.js
@@ -10,23 +10,41 @@ export default class ClasseCarro extends React.Component {
         }
     }
 
-    ligardesligar() {
+    ligardesligar() {  // Seria function ligardesligar() mas em classe se omite a palavra function na definição da função
 
         //this.state.ligado = true; ==> Assim muda só em memória . Pra mudar o estado e renderizar tem que usar o setState
         
-        this.setState({ ligado: !this.state.ligado, velAtual: 10 });
+       // this.setState({ ligado: !this.state.ligado, velAtual: 10 });
+
+        // Ao invez de modificar diretamente o valor do objeto no setState como foi feito acima, uma outra forma é 
+        // passar uma função anônima como argumento do setState como abaixo.
+        // Esta forma é considerada mais segura pois pode ocorrer de ter um delay entre a atualização do state em memória e sua
+        // renderização na tela
+        this.setState(
+            (state) => ({ligado: !state.ligado }) // (ES6) Nesta arrow function é retornado um objeto então o precisa dos parênteses para
+        )                                         // Não confundir as {} com o retorno de função ( que ai precisaria return)
     }
+
+
+    acelerar() {
+
+        this.setState(
+            (state,props) => ({velAtual:state.velAtual + props.fator})
+        );
+    }
+
 
     render() {  // Obrigatório chamar e com o nome render()
         return (
             <div style={{color:"red"}}>
                 <h1> classe carro</h1>
-                Canal: {this.props.canal}  
+                Canal: {this.props.canal} 
                 {this.props.children}
                 <p> Modelo {this.modelo}</p>
                 <p>{this.state.ligado ? "Ligado" : "Desligado"}</p>
                 <p>velocicade:{this.state.velAtual}</p>
-                <button onClick={() => this.ligardesligar() } >Ligar/desligar</button>
+                <button onClick={() => this.ligardesligar()} >Ligar/desligar</button>
+                <button onClick={()=> this.acelerar() }>Acelerar</button>
             </div>
 
             );
