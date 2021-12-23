0cbf385ab6a636700e00935e6e158e70c387367e
adilsonxavier <adilsonxdesouza@gmail.com>
2021-12-23 01:44:50 -0300

    aula23_lifeCycle

---
+++ src/AppCicloVida.js
@@ -0,0 +1,19 @@
+﻿import React from "react";
+/*import ClasseCarroComSpinner from "./componentes/ClasseCarroComSpinner";*/
+//import  ComponentDidMountMethod from "./componentes/lifecycle/ComponentDidMountMethod";
+import ComponentWillUnmount from "./componentes/lifecycle/ComponentWillUnmount";
+
+export default function AppCicloVida() {
+    return (
+        <>
+            {/*<GetDerivedStateFromProps nameFromParent="vim do pai" />*/}
+            {/*<ComponentDidMountMethod />*/}
+            {/* <ComponentDidMountMethodWithFetch />*/}
+            {/*<ShouldComponentUpdateMethod />*/}
+            {/*<GetSnapshotBeforeUpdateMethod />*/}
+            {/*<ComponentDidUpdateMethod />*/}
+            <ComponentWillUnmount/>
+        </>
+    );
+
+}
