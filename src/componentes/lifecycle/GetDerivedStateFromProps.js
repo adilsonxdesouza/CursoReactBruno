071c979c0708ba53b61215986823630a4cab6d43
adilsonxavier <adilsonxdesouza@gmail.com>
2021-12-23 01:41:20 -0300

    aula23_cont_lifecycle

---
+++ src/componentes/lifecycle/GetDerivedStateFromProps.js
@@ -0,0 +1,23 @@
+﻿import React from 'react'
+
+export class GetDerivedStateFromProps extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            name: 'Constructor Method'
+        }
+    }
+
+    static getDerivedStateFromProps(props, state) { // O Métoedo é estatico
+        return { name: props.nameFromParent }       // O médodo getDerivedStateFromProps é opcional e é usado para modificar o estado
+        //return { name: "adilson" }                // antes do render
+    }
+
+    render() {
+        return (
+            <div>
+                This is a {this.state.name}
+            </div>
+        )
+    }
+}
---
+++ src/componentes/lifecycle/ShouldComponentUpdateMethod.js
@@ -0,0 +1,28 @@
+﻿import React, { Component } from 'react'
+
+export default class ShouldComponentUpdateMethod extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            name: 'shouldComponentUpdate Method no construtor'
+        }
+    }
+    shouldComponentUpdate() {   // Este mátodo se retornar false faz com que as alterações de estado feitas em 
+                                // componentDidMount não seja executadas
+                                // Não faz difererça se não tiver componentDidMount
+        return false; //Change to true for state to update
+    }
+
+    //componentDidMount() {
+    //    setTimeout(() => {
+    //        this.setState({ name: "alteração via componentDidMount Method" })
+    //    }, 3000)
+    //}
+    render() {
+        return (
+            <div>
+                <p>This is a {this.state.name}</p>
+            </div>
+        )
+    }
+}
