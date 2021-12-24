2a196c559a32fc17d9767b88c007d39be7bc3fee
adilsonxavier <adilsonxdesouza@gmail.com>
2021-12-24 14:35:36 -0300

    aula28 route nativo

---
+++ src/AppRouteNativo.js
@@ -0,0 +1,45 @@
+﻿import React from "react";
+import Pagina1 from "./componentes/Pagina1";
+import Pagina2 from "./componentes/Pagina2";
+
+export default function AppRouteNativo() {
+
+    const [pagina, setPagina] = React.useState(0);
+
+    React.useEffect(() => {
+        const url = window.location.href;
+        const p = url.split("?");
+        //linkPagina(p[1]);
+        setPagina(p[1]);
+        }
+
+    );
+
+    const linkPagina = (p) => {
+        if (p >= 1) {
+            window.open(`http://localhost:3030?${p}`, "_self");
+        } else {
+            window.open("http://localhost:3030", "_self");
+        } 
+
+    }
+
+    const retornaPagina = () => {
+        if (pagina == 1) { return <Pagina1 /> } else
+            if (pagina == 2) { return <Pagina2 /> } else
+                return (
+                    <>
+                        <button onClick={() => linkPagina(1)}>Pagina1</button>&nbsp;
+                        <button onClick={() => linkPagina(2)}>Pagina2</button>&nbsp;
+                   </>
+                );
+    }
+    return (
+        <>
+            {retornaPagina()}
+        </>
+       
+    );
+
+    
+}
---
+++ src/componentes/Pagina1.js
@@ -0,0 +1,7 @@
+﻿import React from "react";
+
+export default function Pagina1() {
+    return <div><h1>Pagina 1</h1></div> // Posso deixar assim sem parênteses mas tem que estar na mesma linha do return
+        
+        ;
+}
